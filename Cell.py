7bb53df159214828b1f8d58a634bf8bf98f2ae0c
asmor <itoltz@gmail.com>
2013-10-25 06:09:49 -0400

    Finally nailed tile categorization

--- Cell.py
+++ Cell.py
@@ -52,102 +52,26 @@ class Cell:
 
 	@property
 	def isWall(self):
-		"""A wall has exactly one orthogonally-adjacent passable neighbor or exactly two on opposite sides"""
-		if self.passable or self.isCorner:
-			return False
-		up    = self.neighbors[0] != None and self.neighbors[0].passable
-		right = self.neighbors[2] != None and self.neighbors[2].passable
-		down  = self.neighbors[4] != None and self.neighbors[4].passable
-		left  = self.neighbors[6] != None and self.neighbors[6].passable
-
-		if up or down:
-			if left or right:
-				return False
-			return True
-		if left or right:
-			return True
-
+		return masks["wall"].check(self)
 	@property
 	def isCorner(self):
-		"""A corner is has a diagonally-adjacent passable space sandwiched between two orthogonally-adjacent unpassable spaces"""
-		if self.passable:
-			return False
-		for i in range(4):
-			first = i*2
-			second = i*2+1
-			third = (i*2+2) % 8
-			if self.neighbors[first] != None and not self.neighbors[first].passable \
-				and self.neighbors[second] != None and self.neighbors[second].passable \
-				and self.neighbors[third] != None and not self.neighbors[third].passable:
-					return True
-		return False
+		return masks["corner"].check(self)
 
 	@property
 	def isEarth(self):
-		"""Earth is unpassable terrain completely surrounded by unpassable terrain"""
-		if self.passable:
-			return False
-		for cell in self.neighbors:
-			if cell != None and cell.passable:
-				return False
-		return True
+		return masks["earth"].check(self)
 
 	@property
 	def isRoom(self):
-		"""A room is space is either open room, or is passable space adjacent to an open room space"""
-		# Interesting corollary: The first space dug out of a room, that you would intuitively consider a hallway
-		# is considered a room space by this method. It also ends up being an entryway. This has a number of
-		# cascading benefits, e.g. a 2-square "hallway" won't be filled with doors because neither is adjacent
-		# to a hallway!
-		if not self.passable:
-			return False
-		if self.isOpenRoom:
-			return True
-		for cell in self.neighbors:
-			if cell != None and cell.isOpenRoom:
-				return True
-		return False
-
-	@property
-	def isNarrow(self):
-		"""A narrow space has two unpassable spaces on opposite sides of it and isn't a room space"""
-		if not self.passable or self.isRoom:
-			return False
-		for i in range(4):
-			if self.neighbors[i] != None and not self.neighbors[i].passable \
-				and self.neighbors[i+4] != None and not self.neighbors[i+4].passable:
-					return True
-			return False
+		return masks["room"].check(self)
 
 	@property
 	def isOpenRoom(self):
-		"""A room space is open if it isn't adjacent to any walls"""
-		if not self.passable:
-			return False
-		for cell in self.neighbors:
-			if cell != None and not cell.passable:
-				return False
-		return True
-	
-	# @property
-	# def isHallway(self):
-	# 	"""Hallways are narrow terrain and not room spaces"""
-	# 	if not self.isNarrow:
-	# 		return False
-	# 	for cell in self.neighbors:
-	# 		if cell != None and cell.isRoom:
-	# 			return True
-	# 	return False
+		return masks["openroom"].check(self)
 
 	@property
 	def isEntryway(self):
-		"""Entryways are narrow spaces adjacent to rooms"""
-		if not self.isNarrow:
-			return False
-		for cell in self.neighbors[::2]:
-			if cell != None and cell.isRoom:
-				return True
-		return False
+		return masks["entryway"].check(self)
 
 	@property
 	def digCost(self):
@@ -166,6 +90,170 @@ class Cell:
 		# We shouldn't get down here, but if we do...
 		return 10000
 
+class CellMask:
+	"""Given a 3x3 mask, creates all rotational and mirrored versions of that mask"""
+	def __init__(self, mask):
+		self._masks = []
+		self.addMask(mask)
+
+	def addMask(self, mask):
+		rot0 = mask
+		rot90 = zip(*rot0[::-1])
+		rot180 = zip(*rot90[::-1])
+		rot270 = zip(*rot180[::-1])
+		mirrored0 = rot0[::-1]
+		mirrored90 = rot90[::-1]
+		mirrored180 = rot180[::-1]
+		mirrored270 = rot270[::-1]
+
+		if rot0 not in self._masks:
+			self._masks.append(rot0)
+			if rot90 not in self._masks:
+				self._masks.append(rot90)
+				if rot180 not in self._masks:
+					self._masks.append(rot180)
+					# if 90 and 180 are unique, 270 must be unique
+					self._masks.append(rot270)
+
+		if mirrored0 not in self._masks:
+			self._masks.append(mirrored0)
+			if mirrored90 not in self._masks:
+				self._masks.append(mirrored90)
+				if mirrored180 not in self._masks:
+					self._masks.append(mirrored180)
+					# if 90 and 180 are unique, 270 must be unique
+					self._masks.append(mirrored270)
+
+	def check(self, cell):
+		for mask in self._masks:
+			if CellMask._checkSingleMask(mask, cell):
+				return True
+		return False
+
+	@staticmethod
+	def _checkSingleMask(mask, cell):
+		# Check the cell itself, at the center of its pattern
+		if CellMask._checkCellMask(mask[1][1], cell) == False:
+			return False
+
+		# Check each of the cell's neighbors
+		coordsList = [
+			(0, 1, 0), # top
+			(1, 2, 0), # top-right
+			(2, 2, 1), # right
+			(3, 2, 2), # bottom-right
+			(4, 1, 2), # bottom
+			(5, 0, 2), # bottom-left
+			(6, 0, 1), # left
+			(7, 0, 0) # top-left
+		]
+
+		for coords in coordsList:
+			if not CellMask._checkSingleCell(mask, cell, coords):
+				return False
+		return True
+
+	@staticmethod
+	def _checkSingleCell(mask, cell, coords):
+		neighbor = cell.neighbors[coords[0]]
+		cellMask = mask[coords[1]][coords[2]]
+		return CellMask._checkCellMask(cellMask, neighbor)
+
+	@staticmethod
+	def _checkCellMask(cellMask, cell):
+		if cellMask == "?":
+			return True
+
+		if cellMask == "w":
+			return cell != None and not cell.passable
+		if cellMask == "W":
+			return cell == None or cell.passable
+
+		if cellMask == "p":
+			return cell != None and cell.passable
+		if cellMask == "P":
+			return cell == None or not cell.passable
+
+		if cellMask == "r":
+			return cell != None and cell.isRoom
+		if cellMask == "R":
+			return cell == None or not cell.isRoom
+
+		# Got a mask we don't know how to handle
+		return False
+
+'''
+Mask definitions:
+?: ignore
+w: unpassable
+W: not unpassable (i.e. passable or non-existent)
+p: passable
+P: not passable
+r: room
+R: not room
+'''
+masks = {}
+masks["earth"] = CellMask([
+	tuple("PPP"),
+	tuple("PwP"),
+	tuple("PPP")
+])
+masks["room"] = CellMask([
+	tuple("pp?"),
+	tuple("pp?"),
+	tuple("???")
+])
+masks["entryway"] = CellMask([
+	tuple("?r?"),
+	tuple("PpP"),
+	tuple("?p?")
+])
+masks["corner"] = CellMask([ # Concave corner
+	tuple("pP?"),
+	tuple("Pw?"),
+	tuple("???")
+])
+masks["corner"].addMask([ # Convex corner
+	tuple("??p"),
+	tuple("?w?"),
+	tuple("p??")
+])
+masks["wall"] = CellMask([
+	tuple("?p?"),
+	tuple("?w?"),
+	tuple("???")
+])
+masks["openroom"] = CellMask([
+	tuple("ppp"),
+	tuple("ppp"),
+	tuple("ppp")
+])
+
+
 if __name__ == '__main__':
-	print map(str, range(1, 5))
+	maskA = [
+		tuple("x??"),
+		tuple("???"),
+		tuple("?x?")
+	]
+	maskB = [
+		tuple("xxx"),
+		tuple("xxx"),
+		tuple("xxx")
+	]
+
+	print maskB == maskB[::-1]
+	print maskB == zip(*maskB[::-1])
+	print maskB
+	print maskB[::-1]
+	print zip(*maskB[::-1])
+
+	cm = CellMask(maskA)
+	cm.addMask(maskB)
+	# cm = CellMask(maskB)
+
+	for mask in cm._masks:
+		print ""
+		for row in mask:
+			print "".join(row)
 	pass
