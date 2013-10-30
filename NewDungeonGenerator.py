c7ee51ed026de11cf493dc1269f10509fd1a774d
asmor <itoltz@gmail.com>
2013-10-30 18:08:38 -0400

    Room placement now functional

--- NewDungeonGenerator.py
+++ NewDungeonGenerator.py
@@ -1,52 +1,102 @@
+import string
 import random
+import Util
 
 class Dungeon:
 	def __init__(self, targetRooms):
 		self.rooms = []
 		self.spread = int(targetRooms/10)
 		self._fillRooms(targetRooms)
-		spreads = 0
-		while spreads < 10:
-			if self._spreadRooms() < 10:
-				break
-			spreads += 1
+		self._buildMap()
 
 	def _fillRooms(self, targetRooms):
 		for i in range(targetRooms):
-			x = random.randint(-self.spread, self.spread)
-			y = random.randint(-self.spread, self.spread)
-			width = random.randint(1, 5) + random.randint(1, 5)
-			height = random.randint(1, 5) + random.randint(1, 5)
-			self.rooms.append(RoomPlaceholder(width, height, x, y))
+			width = random.randint(2, 5) + random.randint(2, 5)
+			height = random.randint(2, 5) + random.randint(2, 5)
+			self._placeRoom(RoomPlaceholder(width, height))
 
-	def _spreadRooms(self):
-		size = range(len(self.rooms))
-		overlaps = 0
-		comparisons = 0
-		biggestChange = 0
-		for i in size:
-			for j in size[i:]:
-				comparisons += 1
-				vector = self.rooms[i].getOverlapVector(self.rooms[j]);
-				if vector != (0, 0):
-					overlaps += 1
-					self.rooms[i].move(vector)
-					magnitude = abs(vector[0]) + abs(vector[1])
-					if magnitude > biggestChange:
-						print self.rooms[i]
-						print self.rooms[j]
-						print vector
-						biggestChange = magnitude
-		print "Comparisons: %s. Overlaps: %s. Biggest change: %s." % (comparisons, overlaps, biggestChange)
-		return overlaps
+	def _placeRoom(self, room):
+		placed = False
+		while not placed:
+			foundOverlap = False
+			for otherRoom in self.rooms:
+				while room.overlaps(otherRoom):
+					# Push the room out until it no longer overlaps this room
+					room.distance += 1
+					foundOverlap = True
+			if not foundOverlap:
+				self.rooms.append(room)
+				placed = True
 
+	def _buildMap(self):
+		self._getMapSize()
+		self._setupGrid()
+		for room in self.rooms:
+			self._overlayRoom(room)
 
-class RoomPlaceholder:
-	def __init__(self, width, height, x, y):
+	def _getMapSize(self):
+		self.minX = 0;
+		self.minY = 0;
+		self.maxX = 0;
+		self.maxY = 0;
+
+		for room in self.rooms:
+			minX = room.x
+			minY = room.y
+			maxX = room.x + room.width
+			maxY = room.y + room.height
+
+			self.minX = min(self.minX, minX)
+			self.minY = min(self.minY, minY)
+			self.maxX = max(self.maxX, maxX)
+			self.maxY = max(self.maxY, maxY)
+
+		# print "X range: %s - %s. Y range: %s - %s" % (self.minX, self.maxX, self.minY, self.maxY)
+
+	def _setupGrid(self):
+		self.mapWidth = self.maxX - self.minX
+		self.mapHeight = self.maxY - self.minY
+		self.map = [["#" for x in range(self.mapWidth)] for y in range(self.mapHeight)]
+
+	def _overlayRoom(self, room):
+		startX = room.x - self.minX
+		endX = startX + room.width
+		startY = room.y - self.minY
+		endY = startY + room.height
+
+		for x in range(startX, endX):
+			for y in range(startY, endY):
+				if x == startX or y == startY or x+1 == endX or y+1 == endY:
+					self.map[y][x] = "w"
+				else:
+					self.map[y][x] = "."
+
+		# print "Overlaid %s\nX: %s - %s\nY: %s - %s" % (room, startX, endX, startY, endY)
+
+class RoomPlaceholder(object):
+	def __init__(self, width, height):
 		self.width = width
 		self.height = height
-		self.x = x
-		self.y = y
+		self.id = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(8))
+		self.angle = Util.getRandomAngle()
+		self.distance = 0
+
+	@property
+	def distance(self):
+		return self._distance
+	@distance.setter
+	def distance(self, value):
+		self._distance = value
+		coords = Util.getXYByDist(value, self.angle)
+		self._x = coords[0]
+		self._y = coords[1]
+
+	@property
+	def x(self):
+		return self._x
+	@property
+	def y(self):
+		return self._y
 
 	@property
 	def xRange(self):
@@ -55,7 +105,7 @@ class RoomPlaceholder:
 	def yRange(self):
 		return range(self.y, self.y + self.height)
 
-	def getOverlapVector(self, otherRoom):
+	def overlaps(self, otherRoom):
 		xOverlap = 0
 		otherXRange = otherRoom.xRange
 		for x in self.xRange:
@@ -63,7 +113,7 @@ class RoomPlaceholder:
 				xOverlap += 1
 
 		if xOverlap == 0:
-			return (0, 0)
+			return False
 
 		yOverlap = 0
 		otherYRange = otherRoom.yRange
@@ -72,25 +122,40 @@ class RoomPlaceholder:
 				yOverlap += 1
 
 		if yOverlap == 0:
-			return (0, 0)
+			return False
 
-		if self.x < otherRoom.x:
-			xOverlap *= -1
-		if self.y < otherRoom.y:
-			yOverlap *= -1
-
-		return (xOverlap, yOverlap)
-
-	def move(self, vector):
-		self.x += vector[0]
-		self.y += vector[1]
+		# We overlap on both the X and Y axes, so we DO overlap with other room
+		return True
 
 	def __repr__(self):
-		return "[%sx%s] @ (%s, %s)" % (self.width, self.height, self.x, self.y)
-
+		return "%s: [%sx%s] @ (%s, %s) Distance: %s. Angle: %s." % (self.id, self.width, self.height, self.x, self.y, self.distance, self.angle)
 
 if __name__ == "__main__":
 	dungeon = Dungeon(150)
-	# for room in Dungeon.rooms:
-	# 	print room
+	for row in dungeon.map:
+		print "".join(row)
+	print "Map size: %sx%s" % (len(dungeon.map[0]), len(dungeon.map))
+	distances = []
+	for room in dungeon.rooms:
+		distances.append(room.distance)
+
+	def mean(numbers):
+		return sum(numbers) / len(numbers)
+	def median(numbers):
+		numbers.sort()
+		n = len(numbers) 
+		if n & 1:
+			return numbers[n // 2]
+		else:
+			return (numbers[n//2-1] + numbers[n//2]) / 2
+	print max(*distances)
+	print min(*distances)
+	print mean(distances)
+	print median(distances)
+
+	# room = RoomPlaceholder(5,5)
+	# print "%s\n" % room.distance
+	# room.distance = 10
+	# print "%s\n" % room.distance
+	# print room
 	pass
--- Util.py
+++ Util.py
@@ -1,8 +1,16 @@
 import random
 import math
 
-def getRandom(min, max):
-	return int((random.random() * (max + 1 - min)) + min)
+def getXYByDist(distance, direction):
+	"""Given an angle in radians and a distance, gives an integer (x, y) tuple"""
+	y = int(distance * math.sin(direction))
+	x = int(distance * math.cos(direction))
+	return (x, y)
+
+def getRandomAngle():
+	"""Returns a random angle between 0 and 2pi radians"""
+	return random.random() * math.pi * 2
 
 if __name__ == '__main__':
-	print(getRandom(0, 10))
+	print getXYByDist(math.pi*2, 10)
+	pass
